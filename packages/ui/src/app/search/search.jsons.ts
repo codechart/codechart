2a6b24e4e96615c2a69100e213811cd5e281ba30
michael <fmish2@gmail.com>
2019-02-21 11:22:11 +0200

    Update search.jsons.ts

--- packages/ui/src/app/search/search.jsons.ts
+++ packages/ui/src/app/search/search.jsons.ts
@@ -18,6 +18,10 @@ export const specificSearchJsons: PreSearchJson[] = [
     {
         regex: '\\.__TEXT__[^(]',
         name: 'variable usage'
+    },
+    {
+        regex: '\b__TEXT__\b',
+        name: 'exact'
     }
 ]
 

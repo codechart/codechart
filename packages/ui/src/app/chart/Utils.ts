6188631929753e43347f719ec512949add3ace81
michael <fmish2@gmail.com>
2019-07-02 22:47:54 +0300

    correction in utils shade color

--- packages/ui/src/app/chart/Utils.ts
+++ packages/ui/src/app/chart/Utils.ts
@@ -22,9 +22,9 @@ export class Utils {
         G = (G < 255) ? G : 255;
         B = (B < 255) ? B : 255;
 
-        let RR = ((R.toString(16).length === 1) ? "0" + R.toString(16) : R.toString(16));
-        let GG = ((G.toString(16).length === 1) ? "0" + G.toString(16) : G.toString(16));
-        let BB = ((B.toString(16).length === 1) ? "0" + B.toString(16) : B.toString(16));
+        let RR = (R.toString(16).length < 2)?'0'+R.toString(16):R.toString(16);
+        let GG = (G.toString(16).length < 2)?'0'+G.toString(16):G.toString(16);
+        let BB = (B.toString(16).length < 2)?'0'+B.toString(16):B.toString(16);
 
         return "#" + RR + GG + BB;
     }

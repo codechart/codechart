c6669b6ca9da4ac84d8d99f8ac38e36bf5cc3e57
michael <fmish2@gmail.com>
2019-11-02 06:31:18 +0200

    make getContentOfFunction same as in api

--- packages/ui/src/app/chart/Utils.ts
+++ packages/ui/src/app/chart/Utils.ts
@@ -85,48 +85,51 @@ export class Utils {
   }
 
   public static getContentOfFunction(lines: string[], lineIndex: number) {
-    let currentLine = lines[lineIndex];
-    if (currentLine.indexOf('(') === -1) return undefined;
+    let currentLine = lines[lineIndex]
+    if (currentLine.indexOf('(') === -1) return undefined
 
     let countBrackets = (open, close, count, line) => {
-      let openRegex = line.match(new RegExp(`\\${open}`, 'g'));
-      let openCount = !openRegex ? 0 : openRegex.length;
-      let closeRegex = line.match(new RegExp(`\\${close}`, 'g'));
-      let closeCount = !closeRegex ? 0 : closeRegex.length;
-      return count + openCount - closeCount;
-    };
+      let openRegex = line.match(new RegExp(`\\${open}`, 'g'))
+      let openCount = !openRegex ? 0 : openRegex.length
+      let closeRegex = line.match(new RegExp(`\\${close}`, 'g'))
+      let closeCount = !closeRegex ? 0 : closeRegex.length
+      return count + openCount - closeCount
+    }
     let checkLine = (lines: string[], lineIndex, status: 'counting ()' | 'counting {}' | 'after ()' | 'finished', bracketCount, lineCount) => {
-      if (status === 'finished') return undefined;
-      let currentLine = lines[lineIndex];
-      console.log(lineCount, currentLine);
-      let count;
+      if (status === 'finished') return undefined
+      let currentLine = lines[lineIndex]
+      console.log(lineCount, currentLine)
+      let count
       if (status === 'after ()') {
         if (currentLine.match(/^\s*\{/) === null) {
-          checkLine(null, null, 'finished', null, lineCount);
-        } else
-          status = 'counting {}';
+          checkLine(null, null, 'finished', null, lineCount)
+        }
+        else
+          status = 'counting {}'
       }
       if (status === 'counting ()') {
-        count = countBrackets('(', ')', bracketCount, currentLine);
+        count = countBrackets('(', ')', bracketCount, currentLine)
         if (count <= 0) {
           if (currentLine.match('{'))
-            lineCount = checkLine(lines, lineIndex, 'counting {}', 0, lineCount);
+            lineCount = checkLine(lines, lineIndex, 'counting {}', 0, lineCount)
           else
-            lineCount = checkLine(lines, lineIndex + 1, 'after ()', 0, lineCount + 1);
-        } else
-          lineCount = checkLine(lines, lineIndex + 1, 'counting ()', 0, lineCount + 1);
+            lineCount = checkLine(lines, lineIndex + 1, 'after ()', 0, lineCount + 1)
+        }
+        else
+          lineCount = checkLine(lines, lineIndex + 1, 'counting ()', 0, lineCount + 1)
       } else if (status === 'counting {}') {
-        count = countBrackets('{', '}', bracketCount, currentLine);
+        count = countBrackets('{', '}', bracketCount, currentLine)
         if (count <= 0) {
-          return lineCount;
-        } else {
-          lineCount = checkLine(lines, lineIndex + 1, 'counting {}', count, lineCount + 1);
+          return lineCount
+        }
+        else {
+          lineCount = checkLine(lines, lineIndex + 1, 'counting {}', count, lineCount + 1)
         }
       }
-      return lineCount;
-    };
+      return lineCount
+    }
 
-    return checkLine(lines, lineIndex, 'counting ()', 0, 0);
+    return checkLine(lines, lineIndex, 'counting ()', 0, 0)
   }
 
 

881bfa72b3b789458e619f12981aefe29f0a703f
michael <michael.forgacs@verint.com>
2020-08-10 21:03:47 +0300

    correction in end block calculation

--- packages/ui/src/app/chart/Utils.ts
+++ packages/ui/src/app/chart/Utils.ts
@@ -162,7 +162,7 @@ export class Utils {
       console.log(lineCount, currentLine)
       let count
       if (status === 'after ()') {
-        if (currentLine.match(/^\s*{/) === null) {
+        if (currentLine.match(/{\s*$/) === null) {
           checkLine(null, null, 'finished', null, lineCount)
         }
         else

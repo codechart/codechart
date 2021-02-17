6e0920ad14d1a3dafd3125713e2bc4133efb4d8a
michael <michael.forgacs@verint.com>
2021-02-17 23:39:15 +0200

    advanced custom styling

--- packages/ui/src/app/node-styling/node-styling.component.ts
+++ packages/ui/src/app/node-styling/node-styling.component.ts
@@ -1,12 +1,15 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
-import {NodeStyles} from '../chart/chart.consts';
-import {AppComponent} from '../app.component';
+import { Component, ElementRef, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { NodeStyles } from '../chart/chart.consts';
+import { AppComponent } from '../app.component';
+import { JsonPipe } from '@angular/common';
+import { InputTextarea } from 'primeng/primeng';
+import { Utils } from '../chart/Utils';
 
 enum StylingTypes {
   edge, node
 }
 
-const sizeSteps = {start: 20, step: 2}
+const sizeSteps = { start: 20, step: 2 }
 const sizes = [5, 20, 40, 70, 100, 400];
 
 @Component({
@@ -16,17 +19,44 @@ const sizes = [5, 20, 40, 70, 100, 400];
 })
 export class NodeStylingComponent implements OnInit {
   @Input() appComponent: AppComponent;
+  @ViewChild('styleElement') private styleElement: ElementRef
   public sizeSteps = sizeSteps
   public stylingType: StylingTypes = StylingTypes.node
   public StylingTypes = StylingTypes
-
+  public _showAdvanced = false
+  public selecedItemStyle
 
   public sizes = sizes
-  constructor() {
+  constructor(private jsonPipe: JsonPipe) {
   }
 
   ngOnInit() {
   }
+
+  public set showAdvanced(value: boolean) {
+    this._showAdvanced = value
+    let deleteFields = ["id", "d"]
+    if (this.appComponent.selectedNode) {
+      let tempStyle = Utils.deepCopy(this.appComponent.selectedNode)
+      deleteFields.forEach(i => delete tempStyle[i])
+      this.selecedItemStyle = tempStyle
+    }
+    else this.selecedItemStyle = "none"
+  }
+
+  public get showAdvanced(): boolean {
+    return this._showAdvanced
+  }
+
+  public toggleShowAdvanced() {
+    this.showAdvanced = !this.showAdvanced
+  }
+
+  public applyManualStyle() {
+    this.appComponent.chartActions.setSelectionStyle(JSON.parse(this.styleElement.nativeElement.value))
+  }
+
+
 
   groupUngroupFile() {
 

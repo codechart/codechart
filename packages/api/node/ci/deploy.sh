128f64de44b11802b139fecbdaf313734c38017d
yardenshoham <hrsi88@gmail.com>
2021-02-20 21:38:27 +0200

    👷 Deploy in CI via ssh

--- packages/api/node/ci/deploy.sh
+++ packages/api/node/ci/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin $DOCKER_REGISTRY
 docker push "$DOCKER_REGISTRY/$DOCKER_REPOSITORY:$DOCKER_TAG"
+ssh root@192.46.234.106 "docker-compose up -d --pull"

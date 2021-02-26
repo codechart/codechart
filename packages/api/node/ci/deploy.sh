73de399f0b9b7db3bcc279d3e37116e7de386dc4
yardenshoham <hrsi88@gmail.com>
2021-02-26 11:40:26 +0200

    👷 Apply versioning for docker image

--- packages/api/node/ci/deploy.sh
+++ packages/api/node/ci/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin $DOCKER_REGISTRY
 docker push "$DOCKER_REGISTRY/$DOCKER_REPOSITORY:$DOCKER_TAG"
-ssh root@192.46.234.106 "docker-compose pull api && docker-compose up -d"
+docker push "$DOCKER_REGISTRY/$DOCKER_REPOSITORY:latest"
+ssh root@192.46.234.106 "docker-compose pull staging-api && docker-compose up -d"

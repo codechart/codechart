90b1e7c7344da6bccf8f637f6ecad11a57bbca16
yardenshoham <hrsi88@gmail.com>
2021-06-13 22:11:03 +0300

    Add cors

--- packages/license-api/server.go
+++ packages/license-api/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/codechart/license-api/routes/api"
 	"github.com/codechart/license-api/services/v1/audit"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	audit.Init()
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New())
 	app.Mount("/api", api.Handler())
 	app.Listen(":3000")
 }

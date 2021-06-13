d7d251d996519a09e4b87df5ee259a37a3b3c763
michael <michael.forgacs@verint.com>
2021-06-13 18:52:41 +0300

    main page finished

--- packages/landing-page/src/app/app-routing.module.ts
+++ packages/landing-page/src/app/app-routing.module.ts
@@ -3,17 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
 
 const routes: Routes = [
-  //{ path: 'paint', component: PaintImageComponent },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
     component: HomeComponentComponent,
-    data:{header:true}//It will use to specific component if you want to show header or not
-  }  
+    data: {header: true }
+  }
 ];
 
 @NgModule({

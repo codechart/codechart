9bb8205fff7fa71300bc36a61e5f40c4f6521c9f
michael <michael.forgacs@verint.com>
2021-07-02 16:13:06 +0300

    group rectangle color to black

--- packages/ui/src/app/app.component.ts
+++ packages/ui/src/app/app.component.ts
@@ -480,6 +480,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     let fileNodePos = this.chart.getViewPos();
     ChartUtils.setIsCustom(fileNode);
+    fileNode.color = {border: '#000000'}
     this.chart.setLabel(fileNode, 'My Group');
     this.chart.setNodePosition(fileNode, fileNodePos, false);
 

28b52926d1e5294d558c0aa779b519bbcaa700b6
niliproject123 <niliproject123@gmail.com>
2021-12-05 17:55:23 +0200

    selection highlights on the go instead of drawing

--- packages/ui/src/app/chart/area.select.ts
+++ packages/ui/src/app/chart/area.select.ts
@@ -109,9 +109,11 @@ export class AreaSelect {
     this.rect.w = (e.pageX - e.currentTarget.offsetLeft) - this.rect.startX
     this.rect.h = (e.pageY - e.currentTarget.offsetTop) - this.rect.startY
 
+    this.selectNodesFromHighlight();
     // this.ctx.setLineDash([5]);
-    this.ctx.strokeStyle = 'rgb(0, 102, 0)'
-    this.ctx.strokeRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h)
+    // this.selectNodesFromHighlight()
+    // this.ctx.strokeStyle = 'rgb(0, 102, 0)'
+    // this.ctx.strokeRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h)
     // this.ctx.setLineDash([]);
     // this.ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
     // this.ctx.fillRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h);

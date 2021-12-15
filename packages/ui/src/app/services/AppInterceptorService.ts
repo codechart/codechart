e91d39a5e59d06e6944c4bc3789579812ca6a9bc
niliproject123 <niliproject123@gmail.com>
2021-12-15 22:56:32 +0200

    another wtf

--- packages/ui/src/app/services/AppInterceptorService.ts
+++ packages/ui/src/app/services/AppInterceptorService.ts
@@ -20,9 +20,9 @@ export class AppInterceptorsService implements HttpInterceptor {
     return next
       .handle(req).do(event => {
       }, (err: any) => {
+        console.log('rest error', this.counter, req.url, '--')
         if(this.counter>0) this.counter--
         this.app.addMessage('error occured', (err.error && err.error.message) ? err.error.message : "", 3000)
-        console.log('rest error', req.url, '--')
       }, () => {
         console.log('complete', this.counter, req.url, '--')
         if(this.counter>0) this.counter--

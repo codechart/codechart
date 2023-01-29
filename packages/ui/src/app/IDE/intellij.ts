78a01a89009f8ad9a3cf02b241e87611e63337d2
niliproject123 <niliproject123@users.noreply.github.com>
2023-01-29 12:45:26 +0200

    add search management to intellij class

--- packages/ui/src/app/IDE/intellij.ts
+++ packages/ui/src/app/IDE/intellij.ts
@@ -1,7 +1,8 @@
 import { AppComponent } from '../app.component'
 import { ChartUtils } from "../chart/chart.utils";
-import { VisiNode } from "../types.nodejs";
+import { MatchNode, VisiNode } from '../types.nodejs'
 import { SearchActions } from '../search/search.actions'
+import { SearchManagement } from '../SearchManagement'
 
 declare function goToLineIDE(filePath, lineNumber): any
 declare function displayReadmeInIdeCallback(text)
@@ -18,9 +19,17 @@ export const doSomething = () => {doSomethingJS()}
 export class IdeConnect {
   // only for debugging
   public debugIsInIDE
-  public searchActions: SearchActions
+  /////
+  searchManagement: SearchManagement;
+  private searchActions: SearchActions
+  chart: any;
   constructor(private app: AppComponent) {
-    this.searchActions = app.searchActions
+  }
+
+  public initialize() {
+    this.searchActions = this.app.searchActions
+    this.chart = this.app.chart
+    this.searchManagement = this.app.searchManagement
   }
 
   public isInIde(): IDE {
@@ -31,23 +40,29 @@ export class IdeConnect {
 
   public async input_addMatchOnClick(lineNumber, filePath, projectPath) {
     await this.app.setProjectPathAction(projectPath, -1)
-    this.searchActions.addMatchFromFile(this.app.searchObject, filePath, [lineNumber])
+    this.searchActions.addMatchFromFile(this.searchManagement.searchObject, filePath, [lineNumber])
   }
 
   public async input_addFileOnClick(filePath, projectPath) {
     await this.app.setProjectPath(projectPath, -1)
-    this.searchActions.openFile(this.app.searchObject, filePath)
+    this.searchActions.openFile(this.searchManagement.searchObject, filePath)
   }
 
   public input_setTextOfCurrentGroup(content) {
     if(!(ChartUtils.isGroupNode(this.app.selectedNode as VisiNode))) return
+    ChartUtils.setFileContent(this.app.selectedNode, content, this.chart)
   }
 
-  public output_goToLineInIde(filePath, lineNmber) {
-    goToLineIDE(filePath, lineNmber)
+  public output_goToLineInIde(lineNumber) {
+    console.log('going to line')
+    let fileNode = this.app.currentFile.node
+    goToLineIDE(this.searchManagement.getPathByGitUrl(fileNode.d.gitUrl).folder +
+      this.searchManagement.splitChar + fileNode.d.path
+      , lineNumber)
   }
 
   public output_sendContentToIdeReadme(content) {
+    console.log('sending group content to webview')
     displayReadmeInIdeCallback(content)
   }
 }

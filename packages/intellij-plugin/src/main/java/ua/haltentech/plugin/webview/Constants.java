52dfc1b77120c692784ed6b91e51a6508314641b
niliproject123 <niliproject123@users.noreply.github.com>
2024-11-26 11:57:16 +0200

    change name of group in IJ to Covalent

--- packages/intellij-plugin/src/main/java/ua/haltentech/plugin/webview/Constants.java
+++ packages/intellij-plugin/src/main/java/ua/haltentech/plugin/webview/Constants.java
@@ -1,5 +1,5 @@
 package ua.haltentech.plugin.webview;
 
 public final class Constants {
-    public static final String WEBVIEW_MD_NAME = "code-chart-group.md";
+    public static final String WEBVIEW_MD_NAME = "covalent-group-temp.md";
 }

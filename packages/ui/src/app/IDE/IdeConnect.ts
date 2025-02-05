09748590b958b9edbd5fb2a552bc8d553957d916
niliproject123 <niliproject123@users.noreply.github.com>
2025-02-05 21:01:05 +0200

    plugins: remove unnecessary alerts

--- packages/ui/src/app/IDE/IdeConnect.ts
+++ packages/ui/src/app/IDE/IdeConnect.ts
@@ -47,7 +47,6 @@ export class IdeConnect {
 
 
   public async input_addMatchOnClick(lineContent, lineNumber, projectPath, filePath, isReplaceNode) {
-    alert(lineNumber)
     await this.app.synchAction(false)
     const isGitFolder = await this.validateProjectIsGit(projectPath)
     if (!isGitFolder) {
@@ -90,7 +89,6 @@ export class IdeConnect {
   }
 
   public output_goToLineInIde(lineNumber) {
-    alert(lineNumber)
     let fileNode = this.app.currentFile.node
     goToLineInIDE(this.searchManagement.getSelectedProject().localPath, fileNode.d.fileId.path, lineNumber)
   }

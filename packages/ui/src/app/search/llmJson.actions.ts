2dc02f0ef40b082cfda909475eb60e0783c13053
niliproject123 <niliproject123@users.noreply.github.com>
2025-04-23 11:21:10 +0300

    UI: tutorial changes, including llm promt

--- packages/ui/src/app/search/llmJson.actions.ts
+++ packages/ui/src/app/search/llmJson.actions.ts
@@ -1,14 +1,36 @@
 export const LlmToWebviewPrompt = `
-describe your answer as code: a json, an array of objects , in this format:
+## COVALENT DIAGRAMS
+when I ask for a Covalent diagram, AND ONL WHEN I ASK FOR A COVALENT DIAGRAM, follow these instructions:
+### How to a write Covalent diagram
+#### general description
+Here you find instructions how to write a Covalent diagram
+how to write results, and what should be the logic
+
+### how to describe logic
+#### general
+- we want to follow high level logic first, with branches for lower level logic, inner logic, declerations and configurations
+- we want to describe logic as branch in the tree. inner logic goes seperately into a new branch
+- we want a reasonable amount of nodes, not too many, not too few
+- dont go too much into details
+
+#### detailed
+- follow logic, making nodes for method calls, or other actios, linked to each other.
+- inner logic for actions start a new branch
+- each method call is  linked to its implementation, and to the next logical step/action/method
+- method implementations start a new branch of the tree
+- type declarations are connected to where they are used first
+- think of every inner method as another logic branch, repeating same concepts for this branch
+
+#### output format
+
 [{
-id: number // running id,
-label: string // human explanation,
+id: number // running id, starting with 1
+label: string // human explanation, not more than a few words, but meaningful
 filePath: string // relative path to file in project,
 lineContent: string // the actual content of the line to search for
-connectedTo: number // id of node logically previous in flow
+connectedTo: number // id of node logically previous in flow. 0 for first
 lineNumber: number // line number in file, 0 if refering to file as a whole
 }]
-start with id 1, the first node is connected 0
 `
 
 const WebviewToLlmPrompt = `

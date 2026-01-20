1bcbde3e19cc700ea94e4ae4b410693eb18155a7
niliproject123 <niliproject123@users.noreply.github.com>
2026-01-20 21:10:03 +0200

    update user description

--- packages/ui/src/app/search/llmJson.actions.ts
+++ packages/ui/src/app/search/llmJson.actions.ts
@@ -214,7 +214,7 @@ export interface LlmJsonItem {
         const allEdges = this.chartWrapper.getAllEdges(i => true);
         const savedIds: { originalId: string, incremental: number }[] = []        // Get all nodes and filter out filename nodes using ChartUtils
         const allNodes = this.chartWrapper.getAllNodes(i => true).filter(node => {
-            return (this.isNodeExcludedForLlm(node));
+            return (!this.isNodeExcludedForLlm(node));
         }); const resultJson: LlmJsonItem[] = allNodes
             .map((node: VisiNode, index: number) => {
                 savedIds.push({ originalId: node.id as string, incremental: index + 1 });
